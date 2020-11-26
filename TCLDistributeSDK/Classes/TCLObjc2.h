a19c41ff1e6c54d1d7941e580d1891d770a17a86
fansenjun <fansenjun@infinities.com.cn>
2020-11-26 17:44:53 +0800

    add file objc2

---
+++ TCLDistributeSDK/Classes/TCLObjc2.h
@@ -0,0 +1,16 @@
+//
+//  TCLObjc2.h
+//  TCLDistributeSDK
+//
+//  Created by dn on 2020/11/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TCLObjc2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TCLDistributeSDK/Classes/TCLObjc2.m
@@ -0,0 +1,12 @@
+//
+//  TCLObjc2.m
+//  TCLDistributeSDK
+//
+//  Created by dn on 2020/11/26.
+//
+
+#import "TCLObjc2.h"
+
+@implementation TCLObjc2
+
+@end
